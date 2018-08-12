80e99511806a91890c503b209ca05d504a649af4
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-08-12 15:33:29 +0200

    Setup package

---
+++ colored_graph/__init__.py
@@ -0,0 +1,1 @@
+name = "colored_graph"
---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="colored_graph",
+    version="0.1.0",
+    author="Salah Missri",
+    author_email="syrianspock@gmail.com",
+    description="Python tool to draw graphs with colors",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/syrianspock/colored-graph",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "graphviz",
+        "watchdog",
+    ],
+    entry_points={
+        'console_scripts': [
+            'cgraph = colored_graph.colored_graph:main',
+        ],
+    },
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
