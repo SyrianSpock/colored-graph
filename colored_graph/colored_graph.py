a1973f1c3901fe23bef1701ca58e498fd35b12f0
Salah-Eddine Missri <missrisalaheddine@gmail.com>
2018-08-23 21:24:47 +0200

    Parse multiple subgraphs

--- colored_graph/colored_graph.py
+++ colored_graph/colored_graph.py
@@ -38,16 +38,35 @@ def parse_description(description_file):
         header = lines[0] if _is_valid_header(lines[0]) else None
         lines = lines[1:] if header else lines
 
-        lines = list(filter(lambda line: all(symbol not in line.lstrip() for symbol in COMMENT_SYMBOLS), lines))
-        lines = list(map(replace_bad_characters, lines))
-        lines = list(map(lambda line: line.replace('\t', ' ' * 4), lines))
-        tasks = list((line.lstrip(), _depth_level(line)) for line in lines if len(line) > 0)
+        subgraphs = split_subgraphs(lines)
 
-        nodes = list(Node(name=_task_strip(task), color=_task_color(task)) for task, depth in set(tasks))
-        edges = list(Edge(src=_task_strip(src), dst=_task_strip(dst), color=_task_color(src))
-                         for src, dst in set(_node_pairs(tasks, list(), list())))
+        return parse_header(header), subgraphs
 
-        return parse_header(header), nodes, edges
+def split_subgraphs(lines):
+    def _slice(lines, separator):
+        for i, elem in enumerate(lines[1:]):
+            if not elem.startswith(separator):
+                return [lines[:i+1]] + _slice(lines[i+1:], separator)
+        return [lines]
+
+    subgraphs = _slice(lines, ' ')[:-1]
+    subgraphs = list(map(parse_single_graph, subgraphs))
+    return subgraphs
+
+def parse_single_graph(lines):
+    lines = list(filter(lambda line: all(symbol not in line.lstrip() for symbol in COMMENT_SYMBOLS), lines))
+    lines = list(map(replace_bad_characters, lines))
+    lines = list(map(lambda line: line.replace('\t', ' ' * 4), lines))
+    tasks = list((line.lstrip(), _depth_level(line)) for line in lines if len(line) > 0)
+
+    root, tasks = tasks[0], tasks[1:]
+
+    root = Node(name=_task_strip(root[0]), color=_task_color(root[0]))
+    nodes = list(Node(name=_task_strip(task), color=_task_color(task)) for task, depth in set(tasks))
+    edges = list(Edge(src=_task_strip(src), dst=_task_strip(dst), color=_task_color(src))
+                     for src, dst in set(_node_pairs(tasks, list(), list())))
+
+    return root, nodes, edges
 
 def parse_header(header):
     if header is None: return {}
@@ -90,24 +109,35 @@ def _node_pairs(tasks, pairs, parents):
     else:
         return _node_pairs(tasks=tasks[1:], pairs=pairs, parents=[*parents, (child, depth)])
 
-def draw_graph(colors, nodes, edges, format):
+def draw_graph(colors, subgraphs, format):
     graph = Digraph(strict=True, format=format)
-    for node in nodes: _append_node(graph, node, color_palette=colors)
-    for edge in edges: _append_edge(graph, edge, color_palette=colors)
+
+    for subgraph in subgraphs:
+        root, nodes, edges = subgraph
+
+        cluster = Digraph(name='cluster_' + root.name, graph_attr={'label': root.name, 'color': _sanitize_color(root.color, colors)}, node_attr={'rank': 'same'})
+
+        for node in nodes: _append_node(cluster, node, color_palette=colors)
+        for edge in edges: _append_edge(cluster, edge, color_palette=colors)
+
+        graph.subgraph(cluster)
+
     return graph
 
+def _sanitize_color(color, color_palette):
+    return color_palette[color] if color in color_palette.keys() else color
+
 def _append_node(graph, node, color_palette):
-    color = node.color
-    if color in color_palette.keys(): color = color_palette[color]
+    color = _sanitize_color(node.color, color_palette)
     graph.node(node.name, color=color, fontcolor=color)
 
 def _append_edge(graph, edge, color_palette):
-    color = edge.color
-    if color in color_palette.keys(): color = color_palette[color]
+    color = _sanitize_color(edge.color, color_palette)
     graph.edge(edge.src, edge.dst, color=color)
 
 def render_graph(description, view, output_file, format):
-    graph = draw_graph(*parse_description(description), format=format)
+    header, subgraphs = parse_description(description)
+    graph = draw_graph(header, subgraphs, format=format)
 
     output_dir = os.path.dirname(output_file or '')
     output_gv = os.path.join(output_dir, os.path.basename(output_file or 'graph') + '.gv')
